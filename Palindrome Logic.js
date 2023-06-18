3468c0dff5ef6ac4f0cabef8596876dbd8f2528d
NanaVtuber <119906230+NanaVtuber@users.noreply.github.com>
2023-06-18 00:42:34 -0400

    More Tweaks
    background color now changes based on if it is a palindrome or not

--- Palindrome Logic.js
+++ Palindrome Logic.js
@@ -1,7 +1,21 @@
-function palindrome(str) {
-  const trimmed = str.toLowerCase().replace(/[^A-Z0-9]/ig, "")
+function palindrome() {
+  const form = document.getElementById("palindrome").value
+  const trimmed = form.toLowerCase().replace(/[^A-Z0-9]/ig, "")
   const reversed = trimmed.split("").reverse().join("");
-  return reversed === trimmed;
+  const spacecheck = /^\s+$/.test(form)
+  if(spacecheck === true){
+    document.body.style.backgroundColor= "rgb(13, 31, 222)";
+    return document.getElementById("output").innerHTML = "That is not a word"
+  }
+  else if(reversed === trimmed){
+ document.body.style.backgroundColor= "rgb(1, 237, 1)";
+  return document.getElementById("output").innerHTML = "That is a palindrome"
+  }
+  else{
+    document.body.style.backgroundColor= "rgb(219, 8, 8)";
+    return document.getElementById("output").innerHTML = "That is not a palindrome"
+
+  }
   
 }
 
