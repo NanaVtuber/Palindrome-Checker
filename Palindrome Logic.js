0904ef5c7508348f52c45f21ae0846a810a34703
NanaVtuber <119906230+NanaVtuber@users.noreply.github.com>
2023-06-20 01:45:22 -0400

    Added Reset Button

--- Palindrome Logic.js
+++ Palindrome Logic.js
@@ -5,18 +5,29 @@ function palindrome() {
   const spacecheck = /^\s+$/.test(form)
   if(spacecheck === true || form ===""){
     document.body.style.backgroundColor= "rgb(13, 31, 222)";
+    document.getElementById("button").onclick = reset
+    document.getElementById("button").innerHTML = "Reset"
     return document.getElementById("output").innerHTML = "That is not a word"
   }
   else if(reversed === trimmed){
- document.body.style.backgroundColor= "rgb(1, 237, 1)";
+    document.body.style.backgroundColor= "rgb(1, 237, 1)";
+    document.getElementById("button").onclick = reset
+    document.getElementById("button").innerHTML = "Reset"
   return document.getElementById("output").innerHTML = "That is a palindrome"
   }
   else{
     document.body.style.backgroundColor= "rgb(219, 8, 8)";
+    document.getElementById("button").onclick = reset
+    document.getElementById("button").innerHTML = "Reset"
     return document.getElementById("output").innerHTML = "That is not a palindrome"
 
   }
   
 }
-
-palindrome("A man, a plan, a canal. Panama");
+function reset(){
+  document.body.style.backgroundColor= "rgb(13, 31, 222)";
+  document.getElementById("output").innerHTML = "Insert Palindrome"
+  document.getElementById("button").onclick = palindrome
+  document.getElementById("button").innerHTML = "submit"
+  document.getElementById("palindrome").value = ""
+}
